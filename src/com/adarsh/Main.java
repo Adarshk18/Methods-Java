f2e6228e41f6ec8a7a43fe11515d0f74d0578124
FOODOHOLIC <adarshkohli183@gmail.com>
2022-06-23 22:30:41 +0530

    methods

---
+++ src/com/adarsh/Main.java
@@ -0,0 +1,19 @@
+package com.adarsh;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter th second number: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The Sum is: " + sum);
+    }
+}
---
+++ src/com/adarsh/Sum.java
@@ -0,0 +1,22 @@
+package com.adarsh;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        sum();
+    }
+
+    static void sum(){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the first number: ");
+        int num1 = in.nextInt();
+        System.out.print("Enter th second number: ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+
+        System.out.println("The Sum is: " + sum);
+    }
+}
